b5a083f6eefbdf38731cd2da890fc8c4f04109f6
wangshaodi <fengzhong_yue@163.com>
2019-07-02 13:20:59 +0800

    site update:replace 安静写些东西 一点也不安静

--- sedshell.sh
+++ sedshell.sh
@@ -13,26 +13,27 @@ fi
 
 
 function aaa(){
-    sed -i '' 's/董沅鑫/少迪/g' $1
-    sed -i '' 's/网站/小站/g' $1
-    sed -i '' 's/href="\/atom/href="\/theshaodi\/atom/g' $1
-    sed -i '' 's/href="\/image/href="\/theshaodi\/image/g' $1
-    sed -i '' 's/href="\/js/href="\/theshaodi\/js/g' $1
-    sed -i '' 's/href="\/css/href="\/theshaodi\/css/g' $1
-    sed -i '' 's/href="\/styles/href="\/theshaodi\/styles/g' $1
-    sed -i '' 's/href="\/custom/href="\/theshaodi\/custom/g' $1
-    sed -i '' 's/href="\/"/href="\/theshaodi\/"/g' $1
-    sed -i '' 's/href="\/archives/href="\/theshaodi\/archives/g' $1
-    sed -i '' 's/href="\/ategories/href="\/theshaodi\/ategories/g' $1
-    sed -i '' 's/href="\/tags/href="\/theshaodi\/tags/g' $1
-    sed -i '' 's/href="\/friends/href="\/theshaodi\/friends/g' $1
-    sed -i '' 's/href="\/about/href="\/theshaodi\/about/g' $1
-    sed -i '' 's/href="\/passages/href="\/theshaodi\/passages/g' $1
-    sed -i '' 's/href="\/page/href="\/theshaodi\/page/g' $1
+   # sed -i '' 's/董沅鑫/少迪/g' $1
+    #sed -i '' 's/网站/小站/g' $1
+    #sed -i '' 's/href="\/atom/href="\/theshaodi\/atom/g' $1
+    #sed -i '' 's/href="\/image/href="\/theshaodi\/image/g' $1
+    #sed -i '' 's/href="\/js/href="\/theshaodi\/js/g' $1
+    #sed -i '' 's/href="\/css/href="\/theshaodi\/css/g' $1
+    #sed -i '' 's/href="\/styles/href="\/theshaodi\/styles/g' $1
+    #sed -i '' 's/href="\/custom/href="\/theshaodi\/custom/g' $1
+    #sed -i '' 's/href="\/"/href="\/theshaodi\/"/g' $1
+    #sed -i '' 's/href="\/archives/href="\/theshaodi\/archives/g' $1
+    #sed -i '' 's/href="\/ategories/href="\/theshaodi\/ategories/g' $1
+    #sed -i '' 's/href="\/tags/href="\/theshaodi\/tags/g' $1
+    #sed -i '' 's/href="\/friends/href="\/theshaodi\/friends/g' $1
+    #sed -i '' 's/href="\/about/href="\/theshaodi\/about/g' $1
+    #sed -i '' 's/href="\/passages/href="\/theshaodi\/passages/g' $1
+    #sed -i '' 's/href="\/page/href="\/theshaodi\/page/g' $1
 
-    sed -i '' 's/src="\/js/src="\/theshaodi\/js/g' $1
-    sed -i '' 's/src="\/vendor/src="\/theshaodi\/vendor/g' $1
-    sed -i '' 's/src="\/images/src="\/theshaodi\/images/g' $1
+    #sed -i '' 's/src="\/js/src="\/theshaodi\/js/g' $1
+    #sed -i '' 's/src="\/vendor/src="\/theshaodi\/vendor/g' $1
+    #sed -i '' 's/src="\/images/src="\/theshaodi\/images/g' $1
+    sed -i '' 's/安静写些东西/一点也不安静/g' $1
 }
 
 for real_path in $(find $1 -path "*.html")
